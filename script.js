10d3c89ac277905ac83bbd257d41196aeb510e59
[Aniket Yadav] <aniketyadav630671@gmail.com>
2026-06-15 00:36:54 +0530

    Added  sliding buttons and fixed errors and bugs

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-let slides= document.querySelector('.slides');
-let images= document.querySelectorAll('.slides img');
-let prevbtn= document.getElementById('prevbtn');
-let nextbtn= document.getElementById('nextbtn');
-let index= 0;
+let slides = document.querySelector('.slides');
+let images = document.querySelectorAll('.slides img');
+let prevbtn = document.getElementById('prevbtn');
+let nextbtn = document.getElementById('nextbtn');
+let index = 0;
+
 function showSlide(){
-    slides.style.transform= `translateX(${-index*100}%)`;    
+    slides.style.transform = `translateX(${-index * 100}%)`;    
 }
 
 prevbtn.addEventListener('click', function(){
     index--;
-    if(index<0){
-        index= images.length-1;
+    if(index < 0){
+        index = images.length - 1;
     }
     showSlide();
 });
+
 nextbtn.addEventListener('click', function(){
     index++;
-    if(index>=images.length){
-        index=0;
+    if(index >= images.length){
+        index = 0;
     }
     showSlide();
 });
